a5857517e5e3f55ff7f205110201b5e2874230d9
Juansebar <juansebar1991@gmail.com>
2018-05-23 16:58:42 -0400

    Implemented last Acitivty

--- app/src/main/java/com/juanse/swoosh/Controller/BaseActivity.kt
+++ app/src/main/java/com/juanse/swoosh/Controller/BaseActivity.kt
@@ -1,4 +1,4 @@
-package com.juanse.swoosh
+package com.juanse.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/juanse/swoosh/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.juanse.swoosh.Utilities
+
+const val EXTRA_LEAGUE = "EXTRA_LEAGUE"
+const val EXTRA_SKILL_LEVEL = "EXTRA_SKILL_LEVEL"
