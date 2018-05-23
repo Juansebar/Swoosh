4432ce285ee014398399bec5d8dd93d1d18033c2
Juansebar <juansebar1991@gmail.com>
2018-05-23 15:15:31 -0400

    Sending data using intents extra

---
+++ app/src/main/java/com/juanse/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.juanse.swoosh
+
+const val EXTRA_LEAGUE = "LEAGUE"
--- app/src/main/java/com/juanse/swoosh/SkillActivity.kt
+++ app/src/main/java/com/juanse/swoosh/SkillActivity.kt
@@ -2,11 +2,20 @@ package com.juanse.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+    private var league: League? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        // Get the values from Extra
+        val leagueString = intent.getStringExtra(EXTRA_LEAGUE)
+        league = League.valueOf(leagueString)
+
+        println("Selected League: ${league}")
     }
 }
